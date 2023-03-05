4a4de490f4517e9971906ed945df5f0ff092710a
Henry Ifebunandu <henryifebunandu@gmail.com>
2023-03-05 22:16:06 +0100

    first compose UI

---
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -0,0 +1,189 @@
+package com.example.firstcompose
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.firstcompose.ui.theme.FirstComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent() {
+            FirstComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+                ) {
+                    CreateBizCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateBizCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(value = false)
+    }
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 4.dp,
+        ) {
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ImageComponent(Modifier
+                    .size(150.dp)
+                    .padding(5.dp)
+                )
+                Divider()
+                UserInfoComponent()
+                Button(
+                    onClick = {
+                        buttonClickedState.value = !buttonClickedState.value
+                    }) {
+                    Text(
+                        text = "Portfolio",
+                        style = MaterialTheme.typography.button
+                    )
+                }
+
+                if(buttonClickedState.value) {
+                    UserContent()
+                } else {
+                    Box() {}
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserContent() {
+    Box(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .padding(5.dp)) {
+        Surface(modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp,
+                color = Color.LightGray)
+        ) {
+            Portfolio(data = listOf("Project 1", "Project 2", "Project 3"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn(
+        modifier = Modifier.padding(8.dp)
+    ){
+        items(data) {
+            item -> Card(modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp),
+            border = BorderStroke(0.5.dp, Color.LightGray),
+
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                ImageComponent(modifier = Modifier
+                    .size(60.dp)
+                    .padding(5.dp),)
+
+                Column() {
+                    Text(text = item, fontWeight = FontWeight.Bold)
+                    Text(text = "$item long description!")
+                }
+            }
+        }
+        }
+    }
+}
+
+@Composable
+private fun UserInfoComponent() {
+    Column() {
+        Text(
+            text = "Ifebunandu H.",
+            style = MaterialTheme.typography.h4,
+            color = Color.Magenta,
+        )
+        Text(
+            text = "Mobile App Developer",
+            modifier = Modifier.padding(3.dp),
+        )
+        Text(
+            text = "@_maykhid",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.subtitle1,
+        )
+    }
+}
+
+@Composable
+private fun ImageComponent(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "Profile picture",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+
+//@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FirstComposeTheme {
+        CreateBizCard()
+    }
+}
